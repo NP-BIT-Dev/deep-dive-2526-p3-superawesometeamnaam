33e5b0a6378a3d024da0bc367f190a8f98a82313
iKahya <ikahya@st.noorderpoort.nl>
2026-02-09 13:36:51 +0100

    .

--- index.php
+++ index.php
@@ -6,8 +6,24 @@
   <link href="dist/output.css" rel="stylesheet">
 </head>
 <body>
-  <h1 class="text-3xl  font-bold underline">
-    Hello world!
-  </h1>
+   <nav>
+    <div class="itemsNav flex justify-between items-center p-4">
+      <img src="dist/logo/logoArc.png" alt="logoArc" class="h-25 px-5">
+      <div class="btn mr-20">
+        <button type="button" class="bg-[#EA048C] hover:bg-[#c70372]  text-white font-bold px-4 rounded">CONTACT</button>
+      </div>
+    </div>
+   </nav>
+
+    <!-- hero starting -->
+      <section id="hero" class="bg-[#F5F5F5] h-screen flex p-16 justify-center">
+        <div class="backgroundHero  flex justify-center bg-[url('deepdive-3/deep-dive-2526-p3-superawesometeamnaam/dist/img/heroImg.jpg')] bg-cover bg-center w-50 h-50">
+            <!-- <img src="dist/img/heroImg.jpg" alt="heroImg"> -->
+          <h2></h2>
+      </div>
+      </section>
+
+     <!-- hero ending -->
+
 </body>
 </html>
