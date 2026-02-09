72f6fb70a3333437618eed4ece2951e71dd7f65c
iKahya <ikahya@st.noorderpoort.nl>
2026-02-09 12:34:45 +0100

    C:/Program Files/Git/

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-  <meta charset="UTF-8">git config --global --unset credential.helper
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="dist/output.css" rel="stylesheet">
 </head>
