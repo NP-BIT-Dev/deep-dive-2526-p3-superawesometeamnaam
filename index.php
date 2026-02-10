37be861a061584b2c9773f3acdd2099035b1802b
iKahya <ikahya@st.noorderpoort.nl>
2026-02-10 13:55:55 +0100

    .

--- index.php
+++ index.php
@@ -6,9 +6,29 @@
   <link href="dist/output.css" rel="stylesheet">
 </head>
 <body>
-   <nav>
-    <div class="itemsNav flex justify-between items-center p-4">
-      <img src="dist/logo/logoArc.png" alt="logoArc" class="h-25 px-5">
+   <nav class="bg-gradient-to-r from-[#EA048C] via-[#6C6D71] to-[#6C6D71] via-[51%] w-full max-h-40">
+    <div class="itemsNav flex justify-between items-center max-h-30">
+      <div class="relative left-30 top-15">
+        <img src="dist/img/logoWithBg.svg" alt="logoArc" class="h-50">
+      </div>
+      
+      <div class="directions">
+        <ul class="flex">
+          <li>
+            <a href="index.php" class="text-white font-bold px-4 hover:text-[#c70372]">Home</a>
+          </li>
+          <li>
+            <a href="#" class="text-white font-bold px-4 hover:text-[#c70372]">Nieuws</a>
+          </li>
+          <li>
+            <a href="#" class="text-white font-bold px-4 hover:text-[#c70372]">Fotogalerij</a>
+          </li>
+          <li>
+            <a href="#" class="text-white font-bold px-4 hover:text-[#c70372]">Login</a>
+          </li>
+        </ul>
+      </div>
+
       <div class="btn mr-20">
         <button type="button" class="bg-[#EA048C] hover:bg-[#c70372]  text-white font-bold px-4 rounded">CONTACT</button>
       </div>
@@ -17,7 +37,7 @@
 
     <!-- hero starting -->
       <section id="hero" class="bg-[#F5F5F5] h-screen flex p-16 justify-center">
-        <div class="backgroundHero  flex justify-center bg-[url('deepdive-3/deep-dive-2526-p3-superawesometeamnaam/dist/img/heroImg.jpg')] bg-cover bg-center w-50 h-50">
+        <div class="backgroundHero  flex justify-center bg-(image:dist/img/heroImg.jpg) bg-cover bg-center w-50 h-50">
             <!-- <img src="dist/img/heroImg.jpg" alt="heroImg"> -->
           <h2></h2>
       </div>
