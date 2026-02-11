299bd5870b19b8142941c467b4942db22907f43a
Oleksii Khudiakov <okhudiakov@st.noorderpoort.nl>
2026-02-11 15:16:55 +0100

    hero section klaar gemaakt.

--- index.php
+++ index.php
@@ -36,11 +36,28 @@
    </nav>
 
     <!-- hero starting -->
-      <section id="hero" class="bg-[#F5F5F5] h-screen flex p-16 justify-center">
-        <div class="backgroundHero  flex justify-center bg-(image:dist/img/heroImg.jpg) bg-cover bg-center w-50 h-50">
-            <!-- <img src="dist/img/heroImg.jpg" alt="heroImg"> -->
-          <h2></h2>
-      </div>
+      <section id="hero" class="bg-white h-screen flex p-16 justify-center w-full h-full bg-contain bg-center">
+        <div class="backgroundHero  flex justify-center bg-(image:dist/img/heroImg.jpg) relative w-full h-full mt-15 md:mt-40">
+          <img src="dist/img/heroImg.jpg" alt="heroImg" class="absolute inset-0 w-full h-screen object-cover">
+
+          
+          <div class = "absolute inset-0 mt-50 w-full h-50 bg-gray-200/20 py-30 md:py-35 inline-block">
+            <div class="relative z-10 flex flex-col items-center justify-center h-full px-8 md:px-16">
+              <h2 class="text-white text-2xl md:text-5xl font-bold leading-tight drop-shadow-md max-w-2xl opacity-100">
+                Rolschaatsen doe je bij<br>
+                <span class="text-gray-500">ARC</span>
+                <span class="text-[#EA048C]">Noord Nederland</span>
+              </h2>
+
+              <button class="flex justify-center rounded-full w-15 md:w-20 p-[2px] md:p-[5px] bg-white text-white mt-4 mb-4"> </button>
+
+              <p class="text-white text-base md:text-base mt-4 max-w-3xl drop-shadow-sm">
+                Rolschaatsen, plezier, passie, demonstratie, topsport en show daar staan we voor
+              </p>
+            </div>
+          </div>
+
+        </div>
       </section>
 
      <!-- hero ending -->
